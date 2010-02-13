c834ed2b9b279e31d8e9a5077226c1bc896805a9
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2010-02-13 19:21:17 +0100

    Add new Test Cases

--- src/test/java/org/ow2/util/tui/BoundedTextTestCase.java
+++ src/test/java/org/ow2/util/tui/BoundedTextTestCase.java
@@ -1,5 +1,9 @@
 package org.ow2.util.tui;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Guillaume
@@ -8,4 +12,54 @@ package org.ow2.util.tui;
  * To change this template use File | Settings | File Templates.
  */
 public class BoundedTextTestCase {
+
+    private BoundedText text;
+
+    @BeforeTest
+    private void setUp() {
+        text = new BoundedText();
+        text.setSize(10);
+    }
+
+    @Test
+    public void testLeftAlignment() {
+        text.setAlignment(Alignment.LEFT);
+        text.setValue("Hello!");
+
+        StringBuilder sb = new StringBuilder();
+        text.paint(sb);
+        Assert.assertEquals(sb.toString(), "Hello!    ");
+    }
+
+    @Test
+    public void testRightAlignment() {
+        text.setAlignment(Alignment.RIGHT);
+        text.setValue("Hello!");
+
+        StringBuilder sb = new StringBuilder();
+        text.paint(sb);
+        Assert.assertEquals(sb.toString(), "    Hello!");
+    }
+
+    @Test
+    public void testCenterAlignmentRemainingOddNumberOfSpaces() {
+        text.setAlignment(Alignment.CENTER);
+        text.setValue("Hello!");
+
+        StringBuilder sb = new StringBuilder();
+        text.paint(sb);
+        // Exact center is possible
+        Assert.assertEquals(sb.toString(), "  Hello!  ");
+    }
+
+    @Test
+    public void testCenterAlignmentRemainingEvenNumberOfSpaces() {
+        text.setAlignment(Alignment.CENTER);
+        text.setValue("Hello");
+
+        StringBuilder sb = new StringBuilder();
+        text.paint(sb);
+        // Prefer LEFT
+        Assert.assertEquals(sb.toString(), "  Hello   ");
+    }
 }
--- src/test/java/org/ow2/util/tui/processor/CutTextProcessorTestCase.java
+++ src/test/java/org/ow2/util/tui/processor/CutTextProcessorTestCase.java
@@ -1,5 +1,9 @@
 package org.ow2.util.tui.processor;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Guillaume
@@ -8,4 +12,29 @@ package org.ow2.util.tui.processor;
  * To change this template use File | Settings | File Templates.
  */
 public class CutTextProcessorTestCase {
+
+    private CutTextProcessor processor;
+
+    @BeforeTest
+    private void setUp() {
+        processor = new CutTextProcessor();
+    }
+
+    @Test
+    public void testCutValue() {
+        String value = processor.process("A long sentence", 5);
+        Assert.assertEquals(value, "A lon");
+    }
+
+    @Test
+    public void testUnCutValue() {
+        String value = processor.process("Uncut", 10);
+        Assert.assertEquals(value, "Uncut");
+    }
+    
+    @Test
+    public void testUnCutValueSizeEquals() {
+        String value = processor.process("Uncut", 5);
+        Assert.assertEquals(value, "Uncut");
+    }
 }
