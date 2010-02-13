065ec349c09fb59f998dedc28b62e16f2ac5851e
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2010-02-13 19:04:51 +0100

    Add new TestCases

---
+++ src/test/java/org/ow2/util/tui/BoundedTextTestCase.java
@@ -0,0 +1,11 @@
+package org.ow2.util.tui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Guillaume
+ * Date: 13 févr. 2010
+ * Time: 18:45:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class BoundedTextTestCase {
+}
---
+++ src/test/java/org/ow2/util/tui/processor/CutTextProcessorTestCase.java
@@ -0,0 +1,11 @@
+package org.ow2.util.tui.processor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Guillaume
+ * Date: 13 févr. 2010
+ * Time: 18:55:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class CutTextProcessorTestCase {
+}
