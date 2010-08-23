4b2c952d6393cb77467c0d1fea2a06dfc9f9a06e
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2010-08-23 20:32:33 +0200

    Complete Spinner component

--- src/main/java/org/ow2/util/tui/Spinner.java
+++ src/main/java/org/ow2/util/tui/Spinner.java
@@ -1,11 +1,23 @@
+/**
+ * Copyright 2010 OW2 Util
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ow2.util.tui;
 
 /**
- * Created by IntelliJ IDEA.
- * User: guillaume
- * Date: 23 aožt 2010
- * Time: 19:02:23
- * To change this template use File | Settings | File Templates.
+ * A Spinner is a component that display a 1 unit length char that is spinning around it's axis (/-\|).
+ * @author Guillaume Sauthier
  */
 public class Spinner implements ITextualComponent {
 
@@ -14,9 +26,19 @@ public class Spinner implements ITextualComponent {
     private int state = 0;
 
     public void paint(StringBuilder builder) {
+
+        // Print the current char and increment the internal state
         builder.append(spins[state++]);
+
+        // Reset state if required
+        if (state == 4) {
+            state = 0;
+        }
     }
 
+    /**
+     * @return Always return 1.
+     */
     public int getSize() {
         return 1;
     }
--- src/test/java/org/ow2/util/tui/SpinnerTestCase.java
+++ src/test/java/org/ow2/util/tui/SpinnerTestCase.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2010 OW2 Util
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ow2.util.tui;
 
 import org.testng.Assert;
@@ -23,23 +38,36 @@ public class SpinnerTestCase {
     @Test
     public void testSpinner() {
         // first step
-        StringBuilder sb1 = new StringBuilder();
-        spinner.paint(sb1);
-        Assert.assertEquals(sb1.toString(), "/");
+        checkSpinChar("/");
 
         // second step
-        StringBuilder sb2 = new StringBuilder();
-        spinner.paint(sb2);
-        Assert.assertEquals(sb2.toString(), "-");
+        checkSpinChar("-");
 
         // third step
-        StringBuilder sb3 = new StringBuilder();
-        spinner.paint(sb3);
-        Assert.assertEquals(sb3.toString(), "\\");
+        checkSpinChar("\\");
 
         // fourth step
-        StringBuilder sb4 = new StringBuilder();
-        spinner.paint(sb4);
-        Assert.assertEquals(sb4.toString(), "|");
+        checkSpinChar("|");
+    }
+
+    @Test
+    public void testSpinner2() {
+        // first turn
+        checkSpinChar("/");
+        checkSpinChar("-");
+        checkSpinChar("\\");
+        checkSpinChar("|");
+
+        // second turn
+        checkSpinChar("/");
+        checkSpinChar("-");
+        checkSpinChar("\\");
+        checkSpinChar("|");
+    }
+
+    private void checkSpinChar(String expected) {
+        StringBuilder builder = new StringBuilder();
+        spinner.paint(builder);
+        Assert.assertEquals(builder.toString(), expected);
     }
 }
